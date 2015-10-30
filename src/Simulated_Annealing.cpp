d1e7d23247898ed1b22db76184d202a47eea0cd8
dekonunes <dekonunesss@gmail.com>
2015-10-29 23:38:51 -0200

    First commit

---
+++ src/Simulated_Annealing.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Simulated_Annealing.cpp
+// Author      : Deko
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
